41b8e746939477ef95ac41cc7968db244c1fed45
lkhtoan <lkhtoan@gmail.com>
2022-12-03 22:17:38 +0700

    cap nhat tring concatenate

---
+++ StringConcatenation.py
@@ -0,0 +1,9 @@
+print('a' + 'b')
+print('a' + ' ' + 'b')
+print('This' + 'is' + 'a' + 'sentence.')
+print('This' + ' ' + 'is' + ' ' + 'a' + ' ' + 'sentence.')
+print('a' * 1)
+print('a' * 4)
+print('a ' * 5)
+print('a' * 0) # No output for this line
+print('a' * -1) # No output for this line
